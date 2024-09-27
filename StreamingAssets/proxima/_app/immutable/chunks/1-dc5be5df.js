b3f128aa184f06cc853e56e716a0db0fe829f112
Teogai <kamin.k@urniquestudio.com>
2024-09-27 07:24:47 +0700

    first commit

---
+++ StreamingAssets/proxima/_app/immutable/chunks/1-dc5be5df.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-0b4dbec9.js";export{t as component};
---
+++ StreamingAssets/proxima/_app/immutable/chunks/2-c5072f80.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-96f747ba.js";export{t as component};
